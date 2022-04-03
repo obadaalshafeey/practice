3354a46378bc9d18c1b3735bb37ba9ae72685b0d
17-obadaalshafeey <62554357+obadaalshafeey@users.noreply.github.com>
2022-04-03 22:24:39 +0300

    project

---
+++ project/pro.js
@@ -0,0 +1,193 @@
+// 'use strict';
+// // quiz page 
+// ////////////////////////////////////// HTML ///////////////////////////////////////
+// let questions = [
+//     {
+//     numb: 1,
+//     question: "What does HTML stand for?",
+//     answer: "Hyper Text Markup Language",
+//     options: [
+//       "Hyper Text Preprocessor",
+//       "Hyper Text Markup Language",
+//       "Hyper Text Multiple Language",
+//       "Hyper Tool Multi Language"
+//     ]
+//   },
+//     {
+//     numb: 2,
+//     question: "Choose the correct HTML tag for the largest heading?",
+//     answer: "H1",
+//     options: [
+//       "H1",
+//       "Heading",
+//       "Head",
+//       "H6"
+//     ]
+//   },
+//     {
+//     numb: 3,
+//     question: "What are the different levels of headings in HTML?",
+//     answer: "1-6",
+//     options: [
+//       "1-6",
+//       "2-5",
+//       "1-4",
+//       "1-10"
+//     ]
+//   },
+//     {
+//     numb: 4,
+//     question: "Which tag would be used for a line break?",
+//     answer: "Br",
+//     options: [
+//       "Lb",
+//       "Brk",
+//       " Ln",
+//       "Br"
+//     ]
+//   },
+//     {
+//     numb: 5,
+//     question: "How can you make a list that lists the items with numbers?",
+//     answer: "Ol",
+//     options: [
+//       "List",
+//       "Dl",
+//       "Ol",
+//       "Ul"
+//     ]
+//   },
+// ////////////////////////////////////////// CSS ////////////////////////////////////////////
+
+// let questions = [
+//     {
+//     numb: 1,
+//     question: "What does CSS stands for ?",
+//     answer: "Cascading style sheets",
+//     options: [
+//       "Cascade style sheets",
+//       "Color and style sheets",
+//       "Cascading style sheets",
+//       "None of the above"
+//     ]
+//   },
+//     {
+//     numb: 2,
+//     question: "Which of the following is the correct syntax for referring the external style sheet?",
+//     answer: "<link rel="stylesheet" type="text/css" href="example.css">",
+//     options: [
+//       "<style src = example.css>",
+//       "<style src = "example.css" >",
+//       "<stylesheet> example.css </stylesheet>",
+//       "<link rel="stylesheet" type="text/css" href="example.css">"
+//     ]
+//   },
+//     {
+//     numb: 3,
+//     question: "Which of the following CSS property is used to set the background image of an element?",
+//     answer: "background-image",
+//     options: [
+//       "background-attachment",
+//       "background-image",
+//       "background-color",
+//       "None of the above"
+//     ]
+//   },
+//     {
+//     numb: 4,
+//     question: "Which of the following is the correct syntax to display the hyperlinks without any underline?",
+//     answer: "a {text-decoration : none;}",
+//     options: [
+//       "a {text-decoration : underline;}",
+//       "a {decoration : no-underline;}",
+//       " a {text-decoration : none;}",
+//       "None of the above"
+//     ]
+//   },
+//     {
+//     numb: 5,
+//     question: " How to select the elements with the class name "example"?",
+//     answer: ".example",
+//     options: [
+//       "example",
+//       "#example",
+//       ".example",
+//       "Class example"
+//     ]
+//   },
+
+
+// ///////////////////////////////////////// JS ///////////////////////////////////////////
+
+
+// let questions = [
+//     {
+//     numb: 1,
+//     question: "Inside which HTML element do we put the JavaScript?",
+//     answer: "<script>",
+//     options: [
+//       "<scripting>",
+//       "<js>",
+//       "<javascript>",
+//       "<script>"
+//     ]
+//   },
+//     {
+//     numb: 2,
+//     question: "The four basic data types are:",
+//     answer: "strings, numbers, Booleans, and nulls",
+//     options: [
+//       "strings, numbers, BooBoos, and nulls",
+//       "strings, text, Booleans, and nulls",
+//       "strings, numbers, Booleans, and nulls",
+//       "strings, numbers, Booleans, and zeros"
+//     ]
+//   },
+//     {
+//     numb: 3,
+//     question: " What are the types of Pop up boxes available in JavaScript?",
+//     answer: "All of the above",
+//     options: [
+//       "Prompt",
+//       " Alert",
+//       " Confirm",
+//       " All of the above"
+//     ]
+//   },
+//     {
+//     numb: 4,
+//     question: "Which of the following keywords is used to define a variable in Javascript?",
+//     answer: "Both A and B",
+//     options: [
+//       "var",
+//       "let",
+//       "Both A and B",
+//       "None of the above"
+//     ]
+//   },
+//     {
+//     numb: 5,
+//     question: " How do we write a comment in javascript?",
+//     answer: "//",
+//     options: [
+//       "/**/",
+//       "//",
+//       "#",
+//       "$$"
+//     ]
+//   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
